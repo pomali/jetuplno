d7b8198e1b97d6208114ad3d7f3faf46e9cd78d8
pom <stanislavparnicky@gmail.com>
2020-04-05 08:24:07 +0200

    Add CORS, fix lat/long limits,  reformat

--- server/api/jetuplno.py
+++ server/api/jetuplno.py
@@ -1,35 +1,53 @@
 from flask import Blueprint, request
 from flask_restful import Api, Resource
 
-jetuplno_api = Api(Blueprint('jetuplno_api', __name__))  # pylint: disable=invalid-name
 
-LAT_MIN = 16.9
-LAT_MAX = 17.3
-LONG_MIN = 48.0
-LONG_MAX = 48.3
+jetuplno_api = Api(Blueprint("jetuplno_api", __name__))  # pylint: disable=invalid-name
 
-@jetuplno_api.resource('/heatmap-data')
+LAT_MIN = 47
+LAT_MAX = 50
+LONG_MIN = 16.5
+LONG_MAX = 23
+
+
+@jetuplno_api.resource("/heatmap-data")
 class JetuplnoAPI(Resource):
-    
     @staticmethod
     def get():
-        request_data = request.get_json()
-        lat = request_data.get('lat', None)
-        long = request_data.get('long', None)
-        
+        request_data = request.args
+        try:
+            lat = float(request_data.get("lat", None))
+            long = float(request_data.get("long", None))
+        except TypeError:
+            return (
+                {"status": "error", "message": "Geo coordinates missing"},
+                400,
+            )
+
         if lat < LAT_MIN or lat > LAT_MAX or long < LONG_MIN or long > LONG_MAX:
-            return {'status': 'error', 'message': 'Geo coordinates out of boundaries'}, 400
+            return (
+                {
+                    "status": "error",
+                    "message": "Geo coordinates out of boundaries \n lat: {} < {} < {} ; long {} < {} < {} ".format(
+                        LAT_MIN, lat, LAT_MAX, LONG_MIN, long, LONG_MAX
+                    ),
+                },
+                400,
+            )
 
         # TODO logic
-        out = {'status': 'ok', 'lat': lat, 'long': long}
+        out = {
+            "status": "ok",
+            "heatmap": [{"lat": lat, "long": long, "status_value": 100}],
+        }
         return out, 200
-    
+
     @staticmethod
     def post():
         request_data = request.get_json()
-        lat = request_data.get('lat', None)
-        long = request_data.get('long', None)
-        
+        lat = request_data.get("lat", None)
+        long = request_data.get("long", None)
+
         # TODO logic
-        out = {'status': 'ok', 'lat': lat, 'long': long}
+        out = {"status": "ok", "lat": lat, "long": long}
         return out, 200
--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
@@ -9,16 +10,20 @@ def create_app():
     from api.jetuplno import jetuplno_api
     from views.index import index_view
 
-    app = Flask(__name__, template_folder='templates')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'http://localhost:5432/'#os.environ['DATABASE_URL']
+    app = Flask(__name__, template_folder="templates")
 
-    app.register_blueprint(jetuplno_api.blueprint, url_prefix='/api')
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+        "DATABASE_URL", "http://localhost:5432/"
+    )
+
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
+    app.register_blueprint(jetuplno_api.blueprint, url_prefix="/api")
     app.register_blueprint(index_view)
-    
+
     db.init_app(app)
     return app
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = create_app()
     app.run()
